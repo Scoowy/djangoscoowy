1e3db7d60c0da0e4b2a0ad69fd10abf1954c9bce
Scoowy <gahonajuanjo@gmail.com>
2018-02-22 00:47:55 -0500

    Vistas de Post completos

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 
-def listaPost(request):
+def lista_post(request):
     posts = Post.objects.filter(
         fechaPublicacion__lte=timezone.now()).order_by('fechaPublicacion')
-    return render(request, 'blog/listaPost.html', {'posts': posts})
+    return render(request, 'blog/lista_post.html', {'posts': posts})
+
+
+def post_completo(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_completo.html', {'post': post})
