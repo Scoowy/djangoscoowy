3eaf839aa348189a2340a868ff3acbd5151dc935
Scoowy <gahonajuanjo@gmail.com>
2018-02-22 15:03:20 -0500

    Fixed Link Home

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.lista_post),
+    url(r'^$', views.lista_post, name='lista_post'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_completo, name='post_completo'),
 ]
